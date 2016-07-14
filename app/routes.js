5d9a351da5164a21b3095279e23aabc41d4279de
Andrew Brown <andrewbrown600@gmail.com>
2016-07-13 21:15:55 -0700

    functional put command to update restaurant

--- app/routes.js
+++ app/routes.js
@@ -32,4 +32,33 @@ module.exports = function(app, router, mongoose) {
             });
         });//end of post
 
+    //route to get by id, working with single items
+    router.route('/restaurants/:restaurant_id')
+        //will return and display the json for that specific restaurant
+        .get(function(req, res){
+            Restaurant.findById(req.params.restaurant_id, function(err, restaurant){
+                if (err) {
+                  res.send(err);
+                }
+                res.json(restaurant);
+            });
+        })
+
+        //change and override information for this specific restaurant
+        .put(function(req, res){
+            Restaurant.findById(req.params.restaurant_id, function(err, restaurant){
+                if (err) {
+                    res.send(err);
+                }
+                //update restaurant db entry
+                restaurant.name = req.body.name;
+                restaurant.save(function(err){
+                    if (err) {
+                        res.send(err);
+                    }
+                    res.json({payload: 'Restaurant updated!'});
+                });
+            });
+        });
+
 };//end of exports
--- test.sh
+++ test.sh
@@ -2,9 +2,11 @@ echo "posting request..."
 
 param = $1
 
-echo "you entered $1"
+echo "you entered $1 \n"
 
 curl --data "name=$1" http://192.168.33.10:8080/api/restaurants
 
-echo "getting list of all restaurants"
-curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://192.168.33.10:8080/api/restaurants
+echo "getting list of all restaurants \n"
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://192.168.33.10:8080/api/restaurants/5787053e390f72d406fe6c58
+
+curl -i -H "Accept: application/json" -X PUT -d "name=In-n-Out" http://192.168.0.165/persons/person/1  
